c1e7f072337fc0272e2452b5745581b26bae2408
Tribbb Dev <tribbbdev@protonmail.com>
2020-05-26 18:39:03 +0000

    Webpack & dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const GoogleFontsPlugin = require('google-fonts-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
 	module: {
@@ -55,5 +56,6 @@ module.exports = {
 			],
 			formats: ['woff', 'woff2'],
 		}),
+		new Dotenv(),
 	],
 };
