548bce1d056e6c423cb3ae2ef652507bc66423af
Tribbb Dev <tribbbdev@protonmail.com>
2020-09-10 10:38:10 +0000

    Don't show stats when not connected

--- src/components/section/SectionHeroGenesis.jsx
+++ src/components/section/SectionHeroGenesis.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import {Hero, Statistic} from '../ui';
 import { Countdown } from '../ui/Countdown';
 import {Col, Container, Row} from "react-bootstrap";
+import { useEthers } from '../../app';
 
 export function SectionHeroGenesis() {
+	const { connected } = useEthers();
 	return (
 		<section className="section-hero text-center">
 			<Hero />
@@ -19,24 +21,26 @@ export function SectionHeroGenesis() {
 				<a href={process.env.WHITEPAPER_LINK} rel="noreferrer" target="_blank">Read the Whitepaper</a>
 			</div>
 
-			<div className="hero-stats mt-6">
-				<Container fluid="md">
-					<Row className="text-center text-lg-left">
-						<Col lg="3" sm="6" className="mb-4 mb-lg-0">
-							<Statistic id="totalReserve" />
-						</Col>
-						<Col lg="3" sm="6" className="mb-4 mb-lg-0">
-							<Statistic id="getCurrentTokenPrice" />
-						</Col>
-						<Col lg="3" sm="6" className="mb-4 mb-md-0">
-							<Statistic id="averagePrice" />
-						</Col>
-						<Col lg="3" sm="6">
-							<Statistic id="getTotalSupply" />
-						</Col>
-					</Row>
-				</Container>
-			</div>
+			{connected &&
+				<div className="hero-stats mt-6">
+					<Container fluid="md">
+						<Row className="text-center text-lg-left">
+							<Col lg="3" sm="6" className="mb-4 mb-lg-0">
+								<Statistic id="totalReserve"/>
+							</Col>
+							<Col lg="3" sm="6" className="mb-4 mb-lg-0">
+								<Statistic id="getCurrentTokenPrice"/>
+							</Col>
+							<Col lg="3" sm="6" className="mb-4 mb-md-0">
+								<Statistic id="averagePrice"/>
+							</Col>
+							<Col lg="3" sm="6">
+								<Statistic id="getTotalSupply"/>
+							</Col>
+						</Row>
+					</Container>
+				</div>
+			}
 		</section>
 	);
 }
