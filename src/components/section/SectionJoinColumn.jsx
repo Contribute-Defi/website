2cb19c04d7fef95bb6acf6bb16b6eb9ebcd505fc
Anon <anon@gmail.com>
2020-12-22 21:37:32 +0100

    Added different CSS for join-columns phase 2

--- src/components/section/SectionJoinColumn.jsx
+++ src/components/section/SectionJoinColumn.jsx
@@ -4,34 +4,43 @@ import { Link } from 'react-router-dom';
 
 export function SectionJoinColumn() {
 	return (
-		<section className="section-join-contribute">
+		<section className="section-join-contribute-columns">
 			<Container fluid="sm">
 				<Row>
 					<Col className="d-flex justify-content-center">
-						<h1>
+						<h2>
 							Join Contribute DAO by depositing Your <span>trib</span> and securing your rights to mine
 							tdao
-						</h1>
+						</h2>
 					</Col>
 				</Row>
 				<Row className="pointers">
 					<Col>
-						<h2>Locked Liquidity</h2>
+						<h3>Locked Liquidity</h3>
 						<p>
 							By contributing to the Locked Liquidity Event you are essentially exchanging TRIB tokens for
 							a share of 1,000 TRIG. No more TRIG will ever be minted after the end of the event.
 						</p>
 					</Col>
 					<Col>
-						<h2>Mining Tdao</h2>
+						<h3>Mining Tdao</h3>
 						<p>
 							Staked TRIG tokens offer users the ability to earn transfer fees in the form of TDAO, the
 							governance token of Contribute’s ecosystem.
 						</p>
 					</Col>
 					<Col>
-						<h2>Governance Rights</h2>
-						<p>TDAO tokens grant holders full decisional power over the ecosystem and its treasury.</p>
+						<h3>Governance Rights</h3>
+						<p>
+							TDAO tokens grant holders full decisional power over the ecosystem and its treasury.
+							<br />
+							<a
+								href="https://medium.com/contribute-defi/contribute-dao-a-call-for-action-c66e8f5d57b"
+								target="blank"
+							>
+								Learn more here.
+							</a>
+						</p>
 					</Col>
 				</Row>
 			</Container>
